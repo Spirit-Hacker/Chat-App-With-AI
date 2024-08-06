f179ff4a48f9f9f917db7e907f762afcdb2984e4
Pranil Dhutraj <pranildhutraj@gmail.com>
2024-08-06 23:10:50 +0530

    added Send Text Messages Mutation and Emoji Picker

---
+++ convex/messages.ts
@@ -0,0 +1,52 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const sendTextMessages = mutation({
+  args: {
+    sender: v.string(),
+    content: v.string(),
+    conversation: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError(
+        "unauthorization error, while sending text messages",
+      );
+    }
+
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversation = await ctx.db
+      .query("conversations")
+      .filter((q) => q.eq(q.field("_id"), args.conversation))
+      .first();
+
+    if (!conversation) {
+      throw new ConvexError(
+        "Conversation not found, while sending text messages",
+      );
+    }
+
+    if (!conversation.participants.includes(user._id)) {
+      throw new ConvexError("You are not part of this conversation");
+    }
+
+    await ctx.db.insert("messages", {
+      sender: args.sender,
+      content: args.content,
+      conversation: args.conversation,
+      messageType: "text",
+    });
+
+    // TODO: add @gpt check --- later
+  },
+});
---
+++ src/hooks/useComponentVisible.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+
+interface ComponentVisibleHook {
+  ref: React.RefObject<any>;
+  isComponentVisible: boolean;
+  setIsComponentVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const useComponentVisible = (
+  initialIsVisible: boolean,
+): ComponentVisibleHook => {
+  const [isComponentVisible, setIsComponentVisible] =
+    useState(initialIsVisible);
+  const ref = useRef<any>();
+
+  const handleClickOutsize = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutsize, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutsize, true);
+    };
+  });
+
+  return {
+    ref,
+    isComponentVisible,
+    setIsComponentVisible,
+  };
+};
+
+export default useComponentVisible;
